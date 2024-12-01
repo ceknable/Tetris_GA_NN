d36d06c0b750713f7b1e251935af9554bdf5d897
KingRamenXIV <aatassi@nd.edu>
2024-11-30 19:02:57 -0500

    Update Initialize_Board.py

--- Genetic Algorithm/Initialize_Board.py
+++ Genetic Algorithm/Initialize_Board.py
@@ -4,13 +4,16 @@ Created on Sat Nov 30 19:59 2024
 @author: KingRamenXIV
 """
 
-def initialize
-# Create an instance of Tetris
-env = gym.make("tetris_gymnasium/Tetris", render_mode="human")
-env.reset(seed=42)
-
-# Main game loop
-terminated = False
-while not terminated:
-    # Render the current state of the game as text
-    env.render()
+def initialize_board():
+    ''' Function that initializes the tetris board
+    
+    '''
+    
+    # Create an instance of Tetris
+    env = gym.make("tetris_gymnasium/Tetris", render_mode="human")
+    env.reset(seed=42) # Can change the seed
+    
+    terminated = False
+    # Move the first block down one cell
+    action = env.unrwappped.actions.move_down
+    observation, reward, terminated, truncated, info = env.step(action)
