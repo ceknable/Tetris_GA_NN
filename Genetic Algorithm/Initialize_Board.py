7887a3f6dd0244b70f6b56a7ae26603e5203cbd1
KingRamenXIV <aatassi@nd.edu>
2024-11-30 19:06:54 -0500

    Update Initialize_Board.py

--- Genetic Algorithm/Initialize_Board.py
+++ Genetic Algorithm/Initialize_Board.py
@@ -5,7 +5,12 @@ Created on Sat Nov 30 19:59 2024
 """
 
 def initialize_board():
-    ''' Function that initializes the tetris board
+    ''' 
+    Function that initializes the tetris board. 
+
+    Returns: 
+        gameboard: The initilized board 
+        env: The Tetris env to be played in
     
     '''
     
@@ -17,3 +22,8 @@ def initialize_board():
     # Move the first block down one cell
     action = env.unrwappped.actions.move_down
     observation, reward, terminated, truncated, info = env.step(action)
+
+    # Get flattened board to feed to TETRIS
+    gameboard, height, holes = featurize(observation)
+
+    return gameboard, env
